fa9a5321aaf15dbf0cfab4683bf08f82fc472af2
lnoguera <lnoguera@DESKTOP-1SCKMPM>
2020-06-15 16:35:44 -0600

    Added filtering option to films page and added actor details page.

--- postgreSQL/queries.js
+++ postgreSQL/queries.js
@@ -57,6 +57,7 @@ const getFilmById = (film_id) => {
                          ,F.rental_rate
                          ,F.release_year
                          ,AC.first_name || ' ' || AC.last_name as Actor	
+                         ,AC.actor_id
                          ,CT.name as Category
                     FROM film as F INNER JOIN language as L using (language_id)
                          INNER JOIN film_actor as FA using (film_id)
@@ -70,10 +71,33 @@ const getFilmById = (film_id) => {
           .catch((error) => { console.log(error); });
 };
 
+const getFilmsByActorId = (actor_id) => {
+     const query = `SELECT a.actor_id,
+                           a.first_name,
+                           a.last_name,
+                           f.title as film_title,
+                           c.name as category,
+                           f.release_year,
+                           (select cast(count(*) as int) from film_actor where actor_id = a.actor_id) as film_count
+                    FROM actor as a inner join film_actor as fa on a.actor_id = fa.actor_id
+                         inner join film as f on f.film_id = fa.film_id 
+                         inner join film_category as fc on f.film_id = fc.film_id
+                         inner join category as c on c.category_id = fc.category_id
+                    WHERE a.actor_id = ${actor_id}`;
+
+     return db.query(query)
+          .then((data) => (data))
+          .catch((error) => { console.log(error); });
+
+};
+
+
+
 
 module.exports = {
      getCustomers,
      getActors,
      getFilms,
-     getFilmById
+     getFilmById,
+     getFilmsByActorId
 };
